47288286769377d65df45ca067b308f46f4e0dbe
Vuong <vuongit2932000@gmail.com>
2022-04-08 21:13:46 +0700

    update detai page and link pages

---
+++ assets/js/details.js
@@ -0,0 +1,23 @@
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImgp[0].onclick = function()
+{
+    ProductImg.src = SmallImg[0].src;
+}
+SmallImgp[1].onclick = function()
+{
+    ProductImg.src = SmallImg[1].src;
+}
+SmallImgp[2].onclick = function()
+{
+    ProductImg.src = SmallImg[2].src;
+}
+SmallImgp[3].onclick = function()
+{
+    ProductImg.src = SmallImg[3].src;
+}
+SmallImgp[4].onclick = function()
+{
+    ProductImg.src = SmallImg[4].src;
+}
